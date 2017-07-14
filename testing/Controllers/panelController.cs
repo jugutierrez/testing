41a39c67b27e11bf32560d6bd0d8c174ce0a7dcc
juan guillermo gutierrez ascencio <ju.gutierreza@hotmail.com>
2017-07-13 20:19:08 -0400

    barra lateral y reemplazo de divs

---
+++ testing/Controllers/panelController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testing.Controllers
+{
+    public class panelController : Controller
+    {
+        // GET: panel
+        public ActionResult Index()
+        {
+            return PartialView("_index");
+        }
+        public ActionResult agregar_bodega()
+        {
+            return PartialView("_panel_agregar_bodegas");
+        }
+        public ActionResult mis_bodegas()
+        {
+            return PartialView("_panel_mis_ofertas");
+        }
+        public ActionResult informes()
+        {
+            return PartialView("_panel_informes");
+        }
+    }
+}
